27b5270c957514d34bba2d917b4770b874c2ad43
Phearun <phearun.chhun@student.passerellesnumeriques.org>
2021-12-22 12:38:59 +0700

    add button help

--- project-game.py
+++ project-game.py
@@ -4,7 +4,7 @@ root = Tk()
 root.geometry("800x700")
 root.title("Project Game")
 canvas = Canvas(root, width=800, height=800)
-image = ImageTk.PhotoImage(Image.open("C:\\Users\Student\Desktop\Project-game\image\\bg-start.png"))
+image = ImageTk.PhotoImage(Image.open("C:\\Users\Student\Desktop\Phearun\Project-game\image\\bg-start.png"))
 canvas.create_image(0,0, anchor=NW, image = image)
 
 # --------Start Window
@@ -23,35 +23,22 @@ def displayStart():
         print("yes")
     else:
         canvas.create_rectangle(250,420,550,470, fill="white", tags="help")
-        canvas.create_text(400,440, text="Help" ,font=('VNI-Bodon-Poster','30','bold'))
+        canvas.create_text(400,440, text="Help" ,font=('VNI-Bodon-Poster','30','bold'),tags="help")
         print("no")
 displayStart()
+# click help
 def displayHelp(event):
     global toStart
     # root.destroy()
     print("hello")
     toStart = False
     displayStart()
+# ==================================Create  text help==================================================================
+def createHelp():
+    canvas.delete('Exit')
+    canvas.delete('Start')
+    
 canvas.tag_bind("help", "<Button-1>", displayHelp)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 canvas.pack()
 root.mainloop()
 
