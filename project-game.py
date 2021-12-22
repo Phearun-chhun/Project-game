62bc6a36a915de645ea937cbbdaf83d1acee6de9
Phearun <phearun.chhun@student.passerellesnumeriques.org>
2021-12-22 21:43:17 +0700

    add button back

--- project-game.py
+++ project-game.py
@@ -1,13 +1,15 @@
 from tkinter import *
+from tkinter import font
 from PIL import ImageTk, Image
 root = Tk()
 root.geometry("800x700")
 root.title("Project Game")
 canvas = Canvas(root, width=800, height=800)
-bgStart = ImageTk.PhotoImage(Image.open("C:\\Users\Student\Desktop\Project-game\image\\bg-start.png"))
-bgHelp = ImageTk.PhotoImage(Image.open("C:\\Users\Student\Desktop\Project-game\image\\bg_help.gif"))
+# =====================image and background=====================
+bgStart = ImageTk.PhotoImage(Image.open("C:/Users/User/Desktop/Project-game/image/bg-start.png"))
+bgHelp = ImageTk.PhotoImage(Image.open("C:/Users/User/Desktop/Project-game/image/rule.png"))
 
-# --------Start Window
+# =====================Start Window=====================
 displayHomeBg = True
 def displayStart():
     global displayHomeBg
@@ -19,11 +21,19 @@ def displayStart():
         canvas.create_rectangle(300,350,500,400, fill="white", tags="exit", outline="")
         canvas.create_text(400,370, text="Exit" ,font=('VNI-Bodon-Poster','25','bold'))
         canvas.create_rectangle(300,420,500,470, fill="white", tags="help", outline="")
-        canvas.create_text(400,440, text="Help" ,font=('VNI-Bodon-Poster','25','bold'))
+        canvas.create_text(400,440, text="Help" ,font=('VNI-Bodon-Poster','25','bold'),tags='help')
     else:
-        gameRull()
-
-# ----------------- Display help player to paly this game --------------
+        gameRull()       
+        canvas.create_rectangle(300,600,500,670, fill="white", tags="help", outline="")
+        canvas.create_text(400,650,text='BACK',font='Roboto',tags='back')
+#=====================back to window=====================
+def goBack():
+    global displayHomeBg
+    displayHomeBg = True        
+    print('error')
+    displayStart()
+canvas.tag_bind('back','<Button-1>',goBack)
+# ===================== Display help player to paly this game --------------
 def exitFromGame(event):
     root.destroy()
 
@@ -31,7 +41,7 @@ displayStart()
 def gameRull():
     canvas.delete("all")
     canvas.create_image(0,0, anchor=NW, image = bgHelp)
-# click help
+# =====================click help=====================
 def displayHelp(event):
     global displayHomeBg
     displayHomeBg = False
