218b13d5d165eba5a854b0184e4d0494eab32271
Student <Student@PNCA12-02.pnc.lan>
2021-12-22 09:53:30 +0700

    add window background and file sounds

--- project-game.py
+++ project-game.py
@@ -1,13 +1,11 @@
-
-import tkinter as tk
-root = tk.Tk()
-frame = tk.Frame()
-canvas = tk.Canvas(frame)
-# bgImage = tk.PhotoImage(file='image/bg.jpg')
-root.geometry("1240x1024")
-
-canvas.pack(expand=True,fill="both")
-frame.pack(expand= True, fill="both")
+from tkinter import *
+from PIL import ImageTk, Image
+root = Tk()
+root.geometry("800x700")
+canvas = Canvas(root, width=800, height=800)
+image = ImageTk.PhotoImage(Image.open("C:\\Users\Student\Desktop\Project-game\image\\bg-start.png"))
+canvas.create_image(0,0, anchor=NW, image = image)
+canvas.pack()
 root.mainloop()
 
 
