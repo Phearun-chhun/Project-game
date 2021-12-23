0634d51ad3f45a56511ed940f9ffeaa08e6bae55
Phearun <phearun.chhun@student.passerellesnumeriques.org>
2021-12-23 07:41:57 +0700

    change rule

--- project-game.py
+++ project-game.py
@@ -3,9 +3,10 @@ from tkinter import font
 import winsound
 from PIL import ImageTk, Image
 root = Tk()
-root.geometry("800x700")
+root.geometry("800x800")
 root.title("Project Game")
 canvas = Canvas(root, width=800, height=800)
+winsound.PlaySound('.\sound\start-game.wav', winsound.SND_FILENAME)
 # =====================image and background=====================
 bgStart = ImageTk.PhotoImage(Image.open("image/bg-start.png"))
 bgHelp = ImageTk.PhotoImage(Image.open("image/rule.png"))
@@ -17,18 +18,23 @@ def displayStart():
     global displayHomeBg
     canvas.delete("all")
     canvas.create_image(0,0, anchor=NW, image = bgStart)
+    
     if displayHomeBg:
+        # ===========start===========
         canvas.create_rectangle(300,280,500,330, fill="#09b31d", tags="start", outline="")
         canvas.create_text(400,305, text="Start" ,font=('VNI-Bodon-Poster','25','bold'), fill="white")
+        # ===========Exit===========
         canvas.create_rectangle(300,350,500,400, fill="white", tags="exit", outline="")
         canvas.create_text(400,375, text="Exit" ,font=('VNI-Bodon-Poster','25','bold'),tags='exit')
+        # ===========Help===========
         canvas.create_rectangle(300,420,500,470, fill="white", tags="help", outline="")
         canvas.create_text(400,444, text="Help" ,font=('VNI-Bodon-Poster','25','bold'),tags='help')
-        canvas.after(500,displaySound)
+
+        
     else:
         gameRull()       
         canvas.create_rectangle(300,600,500,670, fill="white", tags="help", outline="")
-        canvas.create_text(400,635,text='BACK',font=('Roboto','25','bold'),tags='back')
+        canvas.create_text(400,635,text='BACK',font=('Roboto','23','bold'),tags='back')
 #=====================back to window=====================
 def goBack(event):
     global displayHomeBg
@@ -48,9 +54,8 @@ def displayHelp(event):
     displayHomeBg = False
     displayStart()
 # =====================display sound=====================
-def displaySound():
-    
-    winsound.PlaySound('C:/Users/User/Desktop/Project-game/sound',winsound.SND_FILENAME)
+# def displaySound():   
+
 
 canvas.tag_bind('back','<Button-1>',goBack)
 canvas.tag_bind("exit","<Button-1>", exitFromGame)
